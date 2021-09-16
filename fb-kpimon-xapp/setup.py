c8c3ef8e871a802a38bd4a8e4affb3e51cf206f0
Alan Wang <alanw@fb.com>
2021-09-16 16:19:02 -0700

    Import python kpimon

---
+++ fb-kpimon-xapp/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from setuptools import find_packages, setup
+
+
+setup(
+    name="kpimon",
+    packages=find_packages(),
+    python_requires=">=3.7",
+    install_requires=["onos-ric-sdk-python>=0.1.5-dev"],
+)
