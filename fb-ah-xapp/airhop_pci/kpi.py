482a6f7d13aafafd009dd4b89301d09d45de2009
Ray Milkey <ray@opennetworking.org>
2022-03-08 13:57:23 -0800

    Update FB AH xApp to new KVM service model

--- fb-ah-xapp/airhop_pci/kpi.py
+++ fb-ah-xapp/airhop_pci/kpi.py
@@ -18,7 +18,7 @@ from onos_api.topo import (
     E2Node,
     KpmReportStyle,
 )
-from onos_e2_sm.e2sm_kpm_v2.v2 import (
+from onos_e2_sm.e2smkpmv2.v2 import (
     CellObjectId,
     E2SmKpmActionDefinition,
     E2SmKpmActionDefinitionFormat1,
@@ -82,42 +82,44 @@ async def subscribe(
         cell_idname_map[cell.cell_global_id.value] = idname_map
 
         sub_map[idx + 1] = cell.cell_global_id.value
-        actions.append(
-            Action(
-                id=idx,
-                type=ActionType.ACTION_TYPE_REPORT,
-                subsequent_action=SubsequentAction(
-                    type=SubsequentActionType.SUBSEQUENT_ACTION_TYPE_CONTINUE,
-                    time_to_wait=TimeToWait.TIME_TO_WAIT_ZERO,
-                ),
-                payload=bytes(
-                    E2SmKpmActionDefinition(
-                        ric_style_type=RicStyleType(value=report_style.type),
-                        action_definition_format1=E2SmKpmActionDefinitionFormat1(
-                            cell_obj_id=CellObjectId(value=cell.cell_object_id),
-                            meas_info_list=meas_info_list,
-                            granul_period=GranularityPeriod(value=1000),
-                            subscript_id=SubscriptionId(value=idx + 1),
-                        ),
-                    )
-                ),
-            )
+        action_def = E2SmKpmActionDefinition(
+            ric_style_type=RicStyleType(value=report_style.type),
         )
 
+        format1 = E2SmKpmActionDefinitionFormat1(
+            cell_obj_id=CellObjectId(value=cell.cell_object_id),
+            meas_info_list=meas_info_list,
+            granul_period=GranularityPeriod(value=1000),
+            subscript_id=SubscriptionId(value=idx + 1),
+        )
+
+        action_def.action_definition_formats.action_definition_format1 = format1
+        action_payload = bytes(action_def)
+        logging.warning("format is %s", action_def)
+        action = Action(
+            id=idx,
+            type=ActionType.ACTION_TYPE_REPORT,
+            subsequent_action=SubsequentAction(
+                type=SubsequentActionType.SUBSEQUENT_ACTION_TYPE_CONTINUE,
+                time_to_wait=TimeToWait.TIME_TO_WAIT_ZERO,
+            ),
+            payload=action_payload,
+        )
+        actions.append(
+            action
+        )
     logging.info(f"Subscribing kpm node {e2_node_id}")
 
+    trigger_def = E2SmKpmEventTriggerDefinition()
+    trigger_def.event_definition_formats.event_definition_format1 = E2SmKpmEventTriggerDefinitionFormat1(
+        reporting_period=1000
+    )
     async for (header, message) in e2_client.subscribe(
         e2_node_id=e2_node_id,
         service_model_name="oran-e2sm-kpm",
         service_model_version="v2",
         subscription_id="fb-kpimon_oran-e2sm-kpm_sub",
-        trigger=bytes(
-            E2SmKpmEventTriggerDefinition(
-                event_definition_format1=E2SmKpmEventTriggerDefinitionFormat1(
-                    reporting_period=1000
-                )
-            )
-        ),
+        trigger=bytes(trigger_def),
         actions=actions,
     ):
         logging.debug(f"raw header: {header}")
@@ -132,12 +134,12 @@ async def subscribe(
         ind_header = E2SmKpmIndicationHeader()
         ind_header.parse(header)
         ts = int.from_bytes(
-            ind_header.indication_header_format1.collet_start_time.value, "big"
+            ind_header.indication_header_formats.indication_header_format1.collet_start_time.value, "big"
         )
 
         ind_message = E2SmKpmIndicationMessage()
         ind_message.parse(message)
-        subscript_id = ind_message.indication_message_format1.subscript_id.value
+        subscript_id = ind_message.indication_message_formats.indication_message_format1.subscript_id.value
         cellid = sub_map[subscript_id]
 
         logging.debug(f"cellid:{cellid} hdr:{ind_header} msg:{ind_message}")
@@ -145,8 +147,8 @@ async def subscribe(
         # track metrics per cell
         metrics = {}
         msg_cnt_key = ("global", "kpi_ind_message_count")  # key for msg count
-        meas_info_list = ind_message.indication_message_format1.meas_info_list.value
-        for meas_data_item in ind_message.indication_message_format1.meas_data.value:
+        meas_info_list = ind_message.indication_message_formats.indication_message_format1.meas_info_list.value
+        for meas_data_item in ind_message.indication_message_formats.indication_message_format1.meas_data.value:
             for idx, meas_record_item in enumerate(meas_data_item.meas_record.value):
                 _, metric_value = betterproto.which_one_of(
                     meas_record_item, "measurement_record_item"
